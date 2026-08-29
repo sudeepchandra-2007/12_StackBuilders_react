d037a0064dcf5e54d0846d3b01af8caf8d284103
jaswanthreddy-2007 <jaswanthreddy.d24@iiits.in>
2026-08-29 12:05:00 +0530

    Add download protection security header

--- back-end/src/common/middleware/security.middleware.ts
+++ back-end/src/common/middleware/security.middleware.ts
@@ -10,6 +10,7 @@ export class SecurityHeadersMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction): void {
     res.setHeader('X-Content-Type-Options', 'nosniff');
     res.setHeader('X-Frame-Options', 'DENY');
+    res.setHeader('X-Download-Options', 'noopen');
     res.setHeader('X-XSS-Protection', '1; mode=block');
     res.setHeader('Referrer-Policy', 'no-referrer');
     res.setHeader('Permissions-Policy', 'geolocation=(), microphone=(), camera=()');
