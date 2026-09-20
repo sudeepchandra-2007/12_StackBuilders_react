0a06acd1eb53d009f305b2107ead0fdff0e2a627
Sudeep chandra <sudeepchandra.c24@iiits.in>
2026-09-20 19:17:14 +0530

    Delete back-end directory

--- back-end/src/common/constants/roles.ts
+++
@@ -1,9 +0,0 @@
-export enum Role {
-  Admin = 'Admin',
-  HR = 'HR',
-  Employee = 'Employee',
-  WellnessExpert = 'Wellness Expert',
-}
-
-export const ROLE_HEADER_NAME = 'role';
-
--- back-end/src/experts/dto/update-expert.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateExpertDto } from './create-expert.dto';
-
-export class UpdateExpertDto extends PartialType(CreateExpertDto) {}
-
