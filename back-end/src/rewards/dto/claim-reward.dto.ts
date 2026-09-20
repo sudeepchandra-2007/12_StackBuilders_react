1168d6d21475b3597788c8aef16ff45f75048634
Sudeep chandra <sudeepchandra.c24@iiits.in>
2026-09-20 19:17:14 +0530

    Delete back-end directory

--- back-end/src/rewards/dto/claim-reward.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
-
-export class ClaimRewardDto {
-  @ApiProperty({ example: 'employee-1777718400000-a1b2c3d4' })
-  @IsString()
-  @IsNotEmpty()
-  employeeId: string;
-}
--- back-end/src/wellness-checkins/dto/update-wellness-checkin.dto.ts
+++
@@ -1,7 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateWellnessCheckinDto } from './create-wellness-checkin.dto';
-
-export class UpdateWellnessCheckinDto extends PartialType(
-  CreateWellnessCheckinDto,
-) {}
-
