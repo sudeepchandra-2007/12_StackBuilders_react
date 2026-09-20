92c7e9bd70bac81412dcc2172372336e6b16f73e
Sudeep chandra <sudeepchandra.c24@iiits.in>
2026-09-20 23:24:48 +0530

    Delete 12_StackBuilders directory

--- 12_StackBuilders/back-end/src/hr-profiles/dto/update-hr-profile.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateHrProfileDto } from './create-hr-profile.dto';
-
-export class UpdateHrProfileDto extends PartialType(CreateHrProfileDto) {}
-
